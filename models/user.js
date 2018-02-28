2f3afbfbe72268960817fc3a7236a5c62d9f28bb
anfusion <nzmoriarty@gmail.com>
2018-02-28 21:16:26 +0900

    fix user model 2

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema ({
 	username: {type: String, required: true},
 	password: String,
-	email: {type: String, unique: true, required: true},
+	email: {type: String},
 	resetPasswordToken: String,
 	resetPasswordExpires: Date,
 	isAdmin: {type: Boolean, default: false}
